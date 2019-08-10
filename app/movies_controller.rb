b4f0c2eee9b0694c80e0c5ec5a5097ec5fd81ff6
Jasmine Chen <jasmine1226@gmail.com>
2019-08-10 22:48:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,12 +29,12 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
